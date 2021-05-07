89a4b1e22300baf434e036df5a0cc4821305d94c
Lucas DUPUIS <lucasdupuis7@gmail.com>
2021-05-07 10:15:08 +0200

    refactoring2

--- Scripts/Level1.gd
+++ Scripts/Level1.gd
@@ -1,15 +1,10 @@
 extends Node2D
 
-export var scroll_speed : float = 0.05
 onready var seagullSpawner : PathFollow2D = $SeagullSpawner/PathFollow2D
-onready var fishSpawner : PathFollow2D = $FishSpawner/PathFollow2D
+onready var enemySpawner : PathFollow2D = $EnemySpawner/PathFollow2D
 const SEAGULL : PackedScene = preload("res://Scenes/Seagull.tscn")
-const FISH : PackedScene = preload("res://Scenes/Fish.tscn")
-const BOAT : PackedScene = preload("res://Scenes/Boat.tscn")
+const ENEMY : PackedScene = preload("res://Scenes/Fish.tscn")
 signal score(points)
-
-func _ready():
-	$Clouds.material.set_shader_param("scroll_speed", scroll_speed)
 
 
 func on_Seagull_catched(points):
@@ -19,7 +14,7 @@ func on_Seagull_catched(points):
 func on_Seagull_missed(points):
 	emit_signal("score", points)
 
-func on_Fish_hit(points):
+func on_Enemy_hit(points):
 	emit_signal("score", points)
 
 
@@ -32,15 +27,9 @@ func _on_SeagullRythm_timeout():
 	self.add_child(new_child)
 
 
-func _on_FishRythm_timeout():
-	fishSpawner.offset = rand_range(0, 1600)
-	var new_child : RigidBody2D = FISH.instance()
-	new_child.position = fishSpawner.position
-	new_child.connect("hit", self, "on_Fish_hit")
+func _on_EnemyRythm_timeout():
+	enemySpawner.offset = rand_range(0, 1600)
+	var new_child : RigidBody2D = ENEMY.instance()
+	new_child.position = enemySpawner.position
+	new_child.connect("hit", self, "on_Enemy_hit")
 	self.add_child(new_child)
-
-
-func _on_Boat_gone():
-	var new_boat : Sprite = BOAT.instance()
-	new_boat.connect("gone", self, "_on_Boat_gone")
-	self.add_child(new_boat)
