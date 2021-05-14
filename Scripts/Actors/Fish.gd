2cb4e6956212d2d0d9076f3cde324f2abc963dca
Lucas Dupuis <32811851+Merliin42@users.noreply.github.com>
2021-05-14 10:43:21 +0200

    Adding more fishs

--- Scripts/Actors/Fish.gd
+++ Scripts/Actors/Fish.gd
@@ -2,8 +2,10 @@ extends RigidBody2D
 
 signal hit(points)
 export var points : int = -100
+const animations : Array = ["fish", "flyingfish", "bigfish"]
 
-
+func _ready():
+	$AnimatedSprite.play(animations[int(rand_range(0, 3))])
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
