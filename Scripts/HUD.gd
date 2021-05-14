83dfa94ce1537e983abb1baac1fb283c68ae4fbd
Lucas Dupuis <32811851+Merliin42@users.noreply.github.com>
2021-05-14 11:02:34 +0200

    Changing font

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 
 func change_score(score : int):
-	$VBoxContainer/Score.text = str(score)
+	$MarginContainer/VBoxContainer/Score.text = str(score)
 
 func change_color(color : Color) :
-	$VBoxContainer/Score.add_color_override("font_color", color)
+	$MarginContainer/VBoxContainer/Score.add_color_override("font_color", color)
