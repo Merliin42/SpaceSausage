548c920411954c2352680ca8aa16f52bdf7bab93
Lucas Dupuis <32811851+Merliin42@users.noreply.github.com>
2021-05-15 09:17:34 +0200

    Adding menu + score jucier

--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,8 +1,14 @@
 extends CanvasLayer
 
+var score : int = 0
 
 func change_score(score : int):
-	$MarginContainer/VBoxContainer/Score.text = str(score)
+	$Tween.interpolate_property(self, "score", self.score, score, 1, Tween.TRANS_LINEAR)
+	$Tween.start()
 
 func change_color(color : Color) :
 	$MarginContainer/VBoxContainer/Score.add_color_override("font_color", color)
+
+func _process(delta):
+	$MarginContainer/VBoxContainer/Score.text = str(score)
+
---
+++ Scripts/Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://Scenes/Main.tscn")
+
+
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
